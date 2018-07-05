4bf1710314c325075e08e66287af9b3015d49e8c
Don Mears <Donaldmears1@yahoo.com>
2018-07-05 15:05:48 -0400

    Added CategoryAdapter.java. Deleted unused files.

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -17,6 +17,7 @@ package com.example.android.miwok;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -24,50 +25,20 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
 
-        TextView numbers = (TextView) findViewById(R.id.numbers);
-        TextView family = (TextView) findViewById(R.id.family);
-        TextView colors = (TextView) findViewById(R.id.colors);
-        TextView phrases = (TextView)findViewById(R.id.phrases);
+            // Set the content of the activity to use the activity_main.xml layout file
+            setContentView(R.layout.activity_main);
 
+            // Find the view pager that will allow the user to swipe between fragments
+            ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
-        // same as using onClick in xml
-        // Set a click listener on that View
-        numbers.setOnClickListener(new View.OnClickListener() {
-            // The code in this method will be executed when the numbers View is clicked on.
-            @Override
-            public void onClick(View view) {
-                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
-                startActivity(numbersIntent);
-            }
-        });
+            // Create an adapter that knows which fragment should be shown on each page
+            CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager());
 
-        family.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent numbersIntent = new Intent(MainActivity.this, FamilyActivity.class);
-                startActivity(numbersIntent);
-            }
-        });
-
-        colors.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent numbersIntent = new Intent(MainActivity.this, ColorsActivity.class);
-                startActivity(numbersIntent);
-            }
-        });
-
-        phrases.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent numbersIntent = new Intent(MainActivity.this, PhrasesActivity.class);
-                startActivity(numbersIntent);
-            }
-        });
+            // Set the adapter onto the view pager
+            viewPager.setAdapter(adapter);
+        }
     }
-}
